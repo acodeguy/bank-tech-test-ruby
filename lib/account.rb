6084ba5d35e3cc796b189c2fde0590400070d044
Andrew <24258342+acodeguy@users.noreply.github.com>
2019-04-08 15:55:17 +0100

    add key for transaction hash, tests udpated to cope with these

--- lib/account.rb
+++ lib/account.rb
@@ -16,12 +16,12 @@ class Account
   def deposit(amount:, date:)
     raise 'Cannot deposit negative amounts!' unless amount.positive?
     @balance += amount
-    @transactions.push({date => amount})
+    @transactions.push({date: date, amount: amount})
   end
 
   def withdraw(amount:, date:)
     raise 'Cannot withdraw negative amounts!' unless amount.positive?
     @balance -= amount
-    @transactions.push({date => "#{-amount}"})
+    @transactions.push({date: date, amount: "#{-amount}"})
   end
 end
