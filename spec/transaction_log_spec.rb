fbcc00abc8e1697eb20721e5d4793154007e818e
Andrew <24258342+acodeguy@users.noreply.github.com>
2019-04-09 14:38:01 +0100

    refactor to use double as opposed to actual Transaction

--- spec/transaction_log_spec.rb
+++ spec/transaction_log_spec.rb
@@ -5,7 +5,8 @@ describe TransactionLog do
   it 'can add a new transaction to its items list' do
     trans_log = TransactionLog.new
     expect(trans_log.items.count).to eq 0
-    trans_log.add_item(Transaction.new(date: Date.today, amount: 500, type: :credit, balance: 500))
+    transaction_double = double('Transaction, 500, credit, balance 500')
+    trans_log.add_item(transaction_double)
     expect(trans_log.items.count).to eq 1
   end
 end
