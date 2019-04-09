2d3efad272e0d7fbad367fb429beec2335cb968b
Andrew <24258342+acodeguy@users.noreply.github.com>
2019-04-09 14:41:51 +0100

    add test for sotring default day if left-out of method call

--- spec/transaction_spec.rb
+++ spec/transaction_spec.rb
@@ -9,4 +9,9 @@ describe Transaction do
     expect(transaction.type).to eq :credit
     expect(transaction.balance).to eq 500
   end
+
+  it 'stores the current date if left-off' do
+    transaction_without_a_date = Transaction.new(amount: 250, type: :credit, balance: 250)
+    expect(transaction_without_a_date.date).to eq Date.today
+  end
 end
