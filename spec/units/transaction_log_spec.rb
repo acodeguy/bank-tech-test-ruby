8a378bc704f606c7b44c4f5db112e805d31732c7
Andrew <24258342+acodeguy@users.noreply.github.com>
2019-04-12 16:23:26 +0100

    turn double into one which responds to messages, add test for equality

--- spec/units/transaction_log_spec.rb
+++ spec/units/transaction_log_spec.rb
@@ -4,8 +4,15 @@ describe TransactionLog do
   it 'can add a new transaction to its items list' do
     trans_log = TransactionLog.new
     expect(trans_log.items.count).to eq 0
-    transaction_double = double('Transaction, 500, credit, balance 500')
-    trans_log.add_item(transaction_double)
+    transaction = double('Transaction')
+    allow(transaction).to receive_messages(
+      date: Date.today,
+      type: :credit,
+      amount: 100,
+      balance: 100
+    )
+    trans_log.add_item(transaction)
     expect(trans_log.items.count).to eq 1
+    expect(trans_log.items[0]).to eq transaction
   end
 end
