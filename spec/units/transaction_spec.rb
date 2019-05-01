aa3c60b551fac427e216de4c2b5ccb7d239d5c5c
Andrew <24258342+acodeguy@users.noreply.github.com>
2019-05-01 10:13:55 +0100

    shorten line length for code quality

--- spec/units/transaction_spec.rb
+++ spec/units/transaction_spec.rb
@@ -3,7 +3,7 @@ require 'date'
 
 describe Transaction do
   it 'stores details of a withdrawl or deposit' do
-    transaction = Transaction.new(date: Date.today, amount: 500, type: :credit, balance: 500)
+    transaction = Transaction.new(amount: 500, type: :credit, balance: 500)
     expect(transaction.amount).to eq 500
     expect(transaction.date).to eq Date.today
     expect(transaction.type).to eq :credit
@@ -11,7 +11,7 @@ describe Transaction do
   end
 
   it 'stores the current date if left-off' do
-    transaction_without_a_date = Transaction.new(amount: 250, type: :credit, balance: 250)
-    expect(transaction_without_a_date.date).to eq Date.today
+    transaction = Transaction.new(amount: 250, type: :credit, balance: 250)
+    expect(transaction.date).to eq Date.today
   end
 end
